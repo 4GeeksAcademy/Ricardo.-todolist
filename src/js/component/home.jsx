c43afa260e81fc265affb79034cbc3c81bd00139
ricardodelimaaa <142522306+ricardodelimaaa@users.noreply.github.com>
2023-11-28 15:10:02 +0000

    todolist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tarea, setTarea] = useState("")
+	const [lista, setLista] = useState([])
+	function agregarTarea(e) {
+		e.preventDefault()
+		setLista([...lista, tarea])
+		setTarea("")
+	}
+	function eliminar(id) {
+		let nuevoArray=[]
+		nuevoArray=lista.filter((items,index)=> {
+			if(index != id){
+				return items
+			}
+		})
+		setLista(nuevoArray)
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container">
+			<input className="form-control" type="text" value={tarea} onChange={(e) => setTarea(e.target.value)} />
+			<button className="btn btn-success" onClick={agregarTarea}> Agregar tarea </button>
+			<div>
+				<ul className="list-group">
+					{lista.map((items,id)=> (
+						<li className="list-group-item" key={id}> {items}
+	<button className="btn btn-danger float-end" onClick={()=> eliminar(id)} > x </button>
+
+						 </li>
+					) )}
+
+					 </ul>		</div>
+					 <p> Tarea pendiente:{lista.length}</p>
+
 		</div>
 	);
 };
